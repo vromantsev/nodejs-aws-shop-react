5bb30eabffc025168cce2553a6e9027d4edc71e3
vromantsev <vladyslav.romantsev@fmail.com>
2025-02-24 14:50:37 +0200

    * changed api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://84tkq7of12.execute-api.eu-north-1.amazonaws.com/prod",
+  product: "https://uu2451be7c.execute-api.eu-north-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
