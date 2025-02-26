5742e7726c48c2de4bdfd28c6307b1e2b1259d69
vromantsev <vladyslav.romantsev@fmail.com>
2025-02-26 14:39:14 +0200

    * changed products url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://uu2451be7c.execute-api.eu-north-1.amazonaws.com/prod",
+  product: "https://qm5icqnkq7.execute-api.eu-north-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
